4c269af4741820c8c083a57be7d46ebccd9063f2
sshelton76 <sshelton76@protonmail.com>
2019-01-25 02:21:13 -0700

    Placed management in its own file and put minting into IXToken

---
+++ interfaces/IManaged.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.20;
+
+contract IManaged{
+
+    //Event Fired when a manager is added or removed
+    event ManagerChange(address manager,bool state);
+    
+    event OwnerChange(address owner, bool state);
+
+
+    address owner;
+
+    mapping(address => bool) managers;
+    
+
+    modifier owneronly(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier managed(){
+        require(managers[msg.sender]);
+        _;
+    }
+
+    function addManager(address addr) public owneronly{
+        managers[addr] = true;
+        ManagerChange(addr,true);
+    }
+
+    function removeManager(address addr) public managed{
+        managers[addr] = false;
+        ManagerChange(addr,false);
+    }
+
+}

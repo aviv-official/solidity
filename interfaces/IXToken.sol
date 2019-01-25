54fb1152feaa9dc20ac18391fcf317fe7ea5c76a
sshelton76 <sshelton76@protonmail.com>
2019-01-25 02:17:35 -0700

    Seperated manager interface to encourage reuse, put minting into the IXToken interface

--- interfaces/IXToken.sol
+++ interfaces/IXToken.sol
@@ -1,8 +1,36 @@
 pragma solidity ^0.4.20;
+import "./IManaged.sol";
+import "./IERC20.sol";
 
-contract IXToken /* is ERC20*/ {
+contract IXToken /* is ERC20*/ is IManaged {
+
+    address _estate;
+    //Event Fired when a mint is added or removed
+    event MintChange(address mint, bool state);
+    
+    mapping(address => bool) mints;
+    
+    modifier mintonly(){
+        if(_estate != address(0)){
+            require(IERC20(_estate).balanceOf(msg.sender) >= 1);
+        }
+
+        require(mints[msg.sender]);
+        _;
+    }
+
+    function changeOwner(address _newOwner) public owneronly{
+        OwnerChange(owner,false);
+        OwnerChange(_newOwner,true);
+        owner = _newOwner;
+    }
+
     /**
     * @dev This function allows a contract to handle a deposit of ETH on behalf of another user
     */
     function depositTo(address _to) public payable returns (uint tokens); 
+
+    function mint(uint amount) public;
+    function burn(uint amount) public;
+    function withdraw(uint amount, address dest, address _user) public;
 }
